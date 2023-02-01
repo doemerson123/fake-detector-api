5352022b909f999e45c956fcf8b3aab8187f165f
Donovin Emerson <87036676+doemerson123@users.noreply.github.com>
2023-01-31 23:08:54 -0600

    simplify file for streamlit

--- model_inference.py
+++ model_inference.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import streamlit as st 
 src_path = Path(__file__).parent.parent.parent.resolve()
 #print(str(os.listdir())
-src_path.joinpath(src_path, "api")
+
 
 
 
@@ -21,8 +21,98 @@ src_path.joinpath(src_path, "api")
 #print("this is the path src", str(src_path))
 #sys.path.append('/app/utils')
 
-from app.utils.custom_metrics import StatefullMultiClassFBeta
-from app.utils.load_params import load_params
+class StatefullMultiClassFBeta(Metric):
+
+    """
+    Custom Keras Fbeta metric used to calculate F beta during modeling
+    """
+
+    def __init__(self,
+                name="state_full_binary_fbeta",
+                beta=1,
+                n_class=2,
+                average="macro",
+                epsilon=1e-7,
+                **kwargs):
+
+        # initializing an object of the super class
+        super(StatefullMultiClassFBeta, self).__init__(name=name, **kwargs)
+
+        # initializing state variables
+        # initializing true positives
+        self.tp = self.add_weight(name="tp", shape=(n_class,), initializer="zeros")
+
+        # initializing actual positives
+        self.actual_positives = self.add_weight(
+            name="ap", shape=(n_class,), initializer="zeros"
+        )
+        # initializing predicted positives
+        self.predicted_positives = self.add_weight(
+            name="pp", shape=(n_class,), initializer="zeros"
+        )
+
+        # initializing other atrributes that won't be
+        # changed for every object of this class
+        self.beta_squared = beta**2
+        self.n_class = n_class
+        self.average = average
+        self.epsilon = epsilon
+
+    def update_state(self, ytrue, ypred, sample_weight=None):
+        """
+        Updates states during training
+        """
+
+        # casting ytrue and ypred as float dtype
+        ytrue = tf.cast(ytrue, tf.float32)
+        ypred = tf.cast(ypred, tf.float32)
+
+        # finding the maximum probability in ypred
+        max_prob = tf.reduce_max(ypred, axis=-1, keepdims=True)
+
+        # making ypred one hot encoded so class with the maximum probability
+        # will be encoded as 1 while others as 0
+        ypred = tf.cast(tf.equal(ypred, max_prob), tf.float32)
+
+        # updating true positives atrribute
+        self.tp.assign_add(tf.reduce_sum(ytrue * ypred, axis=0))
+
+        # updating predicted positives atrribute
+        self.predicted_positives.assign_add(tf.reduce_sum(ypred, axis=0))
+
+        # updating actual positives atrribute
+        self.actual_positives.assign_add(tf.reduce_sum(ytrue, axis=0))
+
+    def result(self):
+        """
+        Calculates and returns F beta score
+        """
+
+        self.precision = self.tp / (self.predicted_positives + self.epsilon)
+        self.recall = self.tp / (self.actual_positives + self.epsilon)
+
+        # calculating fbeta score
+        self.fb = ((1 + self.beta_squared) * self.precision * self.recall / \
+            (self.beta_squared * self.precision + self.recall + self.epsilon))
+
+        if self.average == "weighted":
+            return tf.reduce_sum(self.fb * self.actual_positives / \
+                                tf.reduce_sum(self.actual_positives))
+
+        elif self.average == "raw":
+            return self.fb
+
+        return tf.reduce_mean(self.fb)
+
+    def reset_states(self):
+        """
+        Resets all states
+        """
+
+        self.tp.assign(tf.zeros(self.n_class))
+        self.predicted_positives.assign(tf.zeros(self.n_class))
+        self.actual_positives.assign(tf.zeros(self.n_class))
+
 
 # custom metric used during training needed for prediction
 dependencies = {
@@ -30,11 +120,11 @@ dependencies = {
 }
 
 #params = load_params(os.path.join(src_path, 'params.yaml'))
-params = load_params( 'app/params.yaml')
-best_model = params.model_inference.best_model_name
-best_model_location = params.model_inference.best_model_location
+#params = load_params( 'app/params.yaml')
+#best_model = params.model_inference.best_model_name
+#best_model_location = params.model_inference.best_model_location
 
-model = tf.keras.models.load_model(os.path.join(best_model_location, best_model), 
+model = tf.keras.models.load_model(os.path.join(src_path, "best_model.hdf5"), 
                                 custom_objects=dependencies)
 
 def preprocess_image(image):
@@ -103,3 +193,4 @@ def explainable_layers(explainable_image):
 #prediction = fake_detector(best_model_location)
 
 #print(prediction)
+
